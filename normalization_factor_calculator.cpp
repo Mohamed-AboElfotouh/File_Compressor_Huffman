54c95b7aca8ac35c1a9d0661340354454837a2ee
Mohamed-AboElfotouh <mohamed_aboelfotouh@aucegypt.edu>
2025-11-17 00:26:34 +0200

    Create normalization_factor_calculator.cpp

---
+++ normalization_factor_calculator.cpp
@@ -0,0 +1,85 @@
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class PerformanceNormalizer
+{
+private:
+    double normalization_factor;
+
+public:
+    void measureBaseline()
+    {
+
+        int size = 10000;
+        vector<int> arr(size);
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = size - i;
+        }
+
+        auto start = chrono::high_resolution_clock::now();
+
+        // Bubble sort (reference algorithm)
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+        cout << "Baseline time: " << duration << " ms" << endl;
+    }
+
+
+    void setupNormalization(double baseline_time)
+    {
+        int size = 10000;
+        vector<int> arr(size);
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = size - i;
+        }
+
+        auto start = chrono::high_resolution_clock::now();
+
+        // Bubble sort (identical to baseline)
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+
+        auto end = chrono::high_resolution_clock::now();
+        auto device_time = chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        cout << "Test device time: " << device_time << " ms" << endl;
+
+        normalization_factor = baseline_time / device_time;
+
+        cout << "Normalization factor: " << normalization_factor << endl;
+    }
+};
+
+int main()
+{
+    PerformanceNormalizer normalizer;
+
+    normalizer.measureBaseline();
+
+    return 0;
+}
